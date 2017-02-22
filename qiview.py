3fc6aa605661e322357c40dd8286ed63b2124b59
Tobias Wood <tobias@spinicist.org.uk>
2017-02-22 10:46:31 +0000

    ENH: Added proper mouse event handling

--- qiview.py
+++ qiview.py
@@ -21,57 +21,76 @@ PROG_NAME = 'QIView'
 PROG_VERSION = "0.1"
 
 class QICanvas(FigureCanvas):
-    """Canvas to draw slices in."""
+    """Canvas to draw this_slices in."""
 
-    def onclick(self, event):
-        print('button=', event.button, ' x=', event.x, ' y=', event.y, ' xdata=', event.xdata, ' ydata=', event.ydata)
+    def handle_mouse_event(self, event):
+        if (event.button == 1):
+            if (event.inaxes == self.axes[0]):
+                self.cursor[1] = event.ydata
+                self.cursor[2] = event.xdata
+                self.update_figure()
+            elif (event.inaxes == self.axes[1]):
+                self.cursor[0] = event.xdata
+                self.cursor[2] = event.ydata
+                self.update_figure()
+            elif (event.inaxes == self.axes[2]):
+                self.cursor[0] = event.xdata
+                self.cursor[1] = event.ydata
+                self.update_figure()
 
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi, facecolor='k')
+        self.fig = Figure(figsize=(width, height), dpi=dpi, facecolor='k')
 
         gs1 = GridSpec(1, 3)
         gs1.update(left=0.01, right=0.99, bottom=0.16, top=0.99, wspace=0.01, hspace=0.01)
         gs2 = GridSpec(1, 1)
         gs2.update(left=0.08, right=0.92, bottom=0.08, top=0.16, wspace=0.1, hspace=0.1)
-
-        print('Loading')
-        img_mask = nib.load('/Users/Tobias/Data/MATRICS/mouse/mask.nii')
-        img_base = nib.load('/Users/Tobias/Data/MATRICS/mouse/stdWarped.nii.gz')
-        img_color = nib.load('/Users/Tobias/Data/MATRICS/mouse/rlog_jacobian_tstat1.nii.gz')
-        img_alpha = nib.load('/Users/Tobias/Data/MATRICS/mouse/rlog_jacobian_vox_p_fstat1.nii')
-        print('Setup')
-        (tmin, tmax) = np.percentile(img_base.get_data(), (1, 99))
-        (corner1, corner2) = qiplot.findCorners(img_mask)
-
-        cmap = 'RdYlBu_r'
-        print('Setup slice')
-        slices = []
         self.axes = []
-        for direction in ('x', 'y', 'z'):
-            (temp_sl, ext) = qiplot.setupSlice(corner1, corner2, direction, 0.5, 128)
-            slices.append(temp_sl)
-
         for i in range(3):
-            sl_img_mask = qiplot.sampleSlice(img_mask, slices[i], order=1)
-            sl_base = qiplot.applyCM(qiplot.sampleSlice(img_base, slices[i]),
-                                     'gray', (tmin, tmax))
-            sl_color = qiplot.applyCM(qiplot.sampleSlice(img_color, slices[i]), cmap, (-4, 4))
-            sl_alpha = qiplot.scaleAlpha(qiplot.sampleSlice(img_alpha, slices[i]), (0.5, 1.0))
-            sl_blend = qiplot.mask(qiplot.blend(sl_base, sl_color, sl_alpha), sl_img_mask)
-            axes = fig.add_subplot(gs1[i], facecolor='black')
-            axes.imshow(sl_blend, origin='lower', extent=ext, interpolation='hanning')
-            axes.axis('off')
-            axes.axis('image')
-            self.axes.append(axes)
-        self.cbar_axis = fig.add_subplot(gs2[0], facecolor='black')
-        qiplot.alphabar(self.cbar_axis, cmap, (-4, 4), 'T-Stat', (0.5, 1.0), '1 - p')
-        FigureCanvas.__init__(self, fig)
-        FigureCanvas.mpl_connect(self, 'button_press_event', self.onclick)
+            self.axes.append(self.fig.add_subplot(gs1[i], facecolor='black'))
+        self.cbar_axis = self.fig.add_subplot(gs2[0], facecolor='black')
+
+        FigureCanvas.__init__(self, self.fig)
+        FigureCanvas.mpl_connect(self, 'button_press_event', self.handle_mouse_event)
+        FigureCanvas.mpl_connect(self, 'motion_notify_event', self.handle_mouse_event)
         self.setParent(parent)
         FigureCanvas.setSizePolicy(self,
                                    QtWidgets.QSizePolicy.Expanding,
                                    QtWidgets.QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
+
+        print('Loading')
+        self.img_mask = nib.load('/Users/Tobias/Data/MATRICS/mouse/mask.nii')
+        self.img_base = nib.load('/Users/Tobias/Data/MATRICS/mouse/stdWarped.nii.gz')
+        self.img_color = nib.load('/Users/Tobias/Data/MATRICS/mouse/rlog_jacobian_tstat1.nii.gz')
+        self.img_alpha = nib.load('/Users/Tobias/Data/MATRICS/mouse/rlog_jacobian_vox_p_fstat1.nii')
+        (corner1, corner2) = qiplot.findCorners(self.img_mask)
+        self.cursor = (corner1 + corner2) / 2
+        self.update_figure()
+
+    def update_figure(self):
+        (tmin, tmax) = np.percentile(self.img_base.get_data(), (1, 99))
+        (corner1, corner2) = qiplot.findCorners(self.img_mask)
+
+        cmap = 'RdYlBu_r'
+        directions = ('x', 'y', 'z')
+        for i in range(3):
+            (this_slice, sl_extent) = qiplot.setupSlice(corner1, corner2, directions[i], 
+                                                        self.cursor[i], 128, absolute=True)
+            sl_img_mask = qiplot.sampleSlice(self.img_mask, this_slice, order=1)
+            sl_base = qiplot.applyCM(qiplot.sampleSlice(self.img_base, this_slice),
+                                     'gray', (tmin, tmax))
+            sl_color = qiplot.applyCM(qiplot.sampleSlice(self.img_color, this_slice), cmap, (-4, 4))
+            sl_alpha = qiplot.scaleAlpha(qiplot.sampleSlice(self.img_alpha, this_slice), (0.5, 1.0))
+            sl_blend = qiplot.mask(qiplot.blend(sl_base, sl_color, sl_alpha), sl_img_mask)
+            self.axes[i].cla()
+            self.axes[i].imshow(sl_blend, origin='lower', extent=sl_extent, interpolation='hanning')
+            self.axes[i].contour(sl_alpha, (0.95,), origin='lower', extent=sl_extent)
+            self.axes[i].axis('off')
+            self.axes[i].axis('image')
+
+        qiplot.alphabar(self.cbar_axis, cmap, (-4, 4), 'T-Stat', (0.5, 1.0), '1 - p')
+        self.draw()
 
 class ApplicationWindow(QtWidgets.QMainWindow):
     def __init__(self):
