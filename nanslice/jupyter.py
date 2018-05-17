3b52e9d41559750498ba8e38faa76f273782fe20
Tobias Wood <tobias@spinicist.org.uk>
2018-05-17 12:22:33 +0100

    WIP: Found magic incantions to make widgets work

--- nanslice/jupyter.py
+++ nanslice/jupyter.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import ipympl
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import ipywidgets as ipy
 from . import util, image
 from .box import Box
 from .slicer import Slicer
@@ -32,7 +34,7 @@ def slices(img, ncols=3, nrows=1, axis='z', lims=(0.1, 0.9),
         axes.axis('off')
         if contour_img:
             sl_contour = slr.sample(contour_img, order=1)
-            ax.contour(sl_contour, levels=contour_values, origin='lower', extent=slr.extent,
+            axes.contour(sl_contour, levels=contour_values, origin='lower', extent=slr.extent,
                        colors=contour_colors, linestyles=contour_styles, linewidths=1)
 
     if cmap:
@@ -83,6 +85,7 @@ def three_plane(img,
         bbox = Box.fromImage(base.image)
     if cmap:
         fig, axes = plt.subplots(1, 4, figsize=(12, 3), facecolor='black')
+        util.colorbar(axes[3], base.cmap, base.clim, label, black_backg=True, orient='v')
     else:
         fig, axes = plt.subplots(1, 3, figsize=(9, 3), facecolor='black')
     for i in range(3):
@@ -90,59 +93,48 @@ def three_plane(img,
         blended_slice = overlay(slr, base, overlays, 1)
         axes[i].imshow(blended_slice, origin='lower', extent=slr.extent, interpolation='nearest')
         axes[i].axis('off')
-    if cmap:
-        util.colorbar(axes[3], base.cmap, base.clim, label, black_backg=True, orient='v')
     fig.tight_layout()
     plt.close()
     return fig
 
-def interactive(img, img_cmap='gray', img_window=(2, 98), mask=None,
-                color_img=None, color_cmap='viridis', color_window=None, color_thresh=None,
-                alpha_img=None, alpha_window=None,
+def three_plane_viewer(img,
+                cmap=None, clim=None, label='',
+                mask=None, overlays=None,
                 orient='clin', samples=128):
-    import ipywidgets as ipy
-
-    # Get some information about the image
+    """Standard 3-plane view with sliders to control X, Y, Z"""
+    plt.ion()
+    base = Layer(img, cmap=cmap, clim=clim, mask=mask)
     if mask:
-        bbox = Box.fromMask(mask)
+        bbox = Box.fromMask(base.mask_image)
     else:
-        bbox = Box.fromImage(img)
-    window_vals = np.nanpercentile(img.get_data(), img_window)
-    # Setup figure
-    fig, axes = plt.subplots(1, 3, figsize=(9, 3), facecolor='r')
+        bbox = Box.fromImage(base.image)
+    if cmap:
+        fig, axes = plt.subplots(1, 4, figsize=(12, 3), facecolor='black')
+        util.colorbar(axes[3], base.cmap, base.clim, label, black_backg=True, orient='v')
+    else:
+        fig, axes = plt.subplots(1, 3, figsize=(8, 3), facecolor='black')
     implots = [None, None, None]
-    init = False
-
-    if color_img and color_window is None:
-        color_window = np.nanpercentile(color_img.get_data(), (2, 98))
-    if alpha_img and alpha_window is None:
-        alpha_window = np.nanpercentile(alpha_img.get_data(), (2, 98))
-    
-    options = util.Options(interp_order=0, color_map=color_cmap, color_lims=color_window, color_scale=1,
-                      color_mask_thresh=color_thresh,
-                      alpha_lims=alpha_window)
-
     def wrap_sections(X, Y, Z):
         for i in range(3):
-            sl = Slicer(bbox, (X, Y, Z), i, samples=samples, orient=orient)
-            sl_final = util.overlay_slice(sl, options, window_vals,
-                                          img, mask, color_img, None, alpha_img)
-            if init:
-                implots[i].set_data(sl_final)
-                # plt.show()
+            slr = Slicer(bbox, (X, Y, Z), i, samples=samples, orient=orient)
+            blended_slice = overlay(slr, base, overlays, 1)
+            if implots[i]:
+                implots[i].set_data(blended_slice)
             else:
-                implots[i] = axes[i].imshow(sl_final, origin='lower', extent=sl.extent,
-                                            interpolation='nearest')
+                implots[i] = axes[i].imshow(blended_slice, origin='lower',
+                                            extent=slr.extent, interpolation='nearest')
                 axes[i].axis('off')
-    
+                plt.show()
     wrap_sections(bbox.center[0], bbox.center[1], bbox.center[2])
     fig.tight_layout()
     # Setup widgets
-    slider_x = ipy.FloatSlider(min=bbox.start[0], max=bbox.end[0], value=bbox.center[0], continuous_update=True)
-    slider_y = ipy.FloatSlider(min=bbox.start[1], max=bbox.end[1], value=bbox.center[1], continuous_update=True)
-    slider_z = ipy.FloatSlider(min=bbox.start[2], max=bbox.end[2], value=bbox.center[2], continuous_update=True)
+    slider_x = ipy.FloatSlider(min=bbox.start[0], max=bbox.end[0], value=bbox.center[0],
+                               continuous_update=True)
+    slider_y = ipy.FloatSlider(min=bbox.start[1], max=bbox.end[1], value=bbox.center[1],
+                               continuous_update=True)
+    slider_z = ipy.FloatSlider(min=bbox.start[2], max=bbox.end[2], value=bbox.center[2],
+                               continuous_update=True)
     widgets = ipy.interactive(wrap_sections, X=slider_x, Y=slider_y, Z=slider_z)
-
     # Now do some manual layout
     hbox = ipy.HBox(widgets.children[0:3]) # Set the sliders to horizontal layout
     vbox = ipy.VBox((hbox, widgets.children[3]))
