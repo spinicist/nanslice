7ba79d4997fb4a56f495a925720275db733d358a
Tobias Wood <tobias@spinicist.org.uk>
2019-01-11 14:53:41 +0000

    STYLE: Ran formatter

--- nanslice/slicer.py
+++ nanslice/slicer.py
@@ -8,9 +8,11 @@ image arrays that can be drawn with matlplotlib.
 import numpy as np
 import scipy.ndimage.interpolation as ndinterp
 
-Axis_map = {'x':0, 'y':1, 'z':2}
+Axis_map = {'x': 0, 'y': 1, 'z': 2}
 Orient_map = {'clin': ({0: 1, 1: 0, 2: 0}, {0: 2, 1: 2, 2: 1}),
               'preclin': ({0: 2, 1: 2, 2: 0}, {0: 1, 1: 0, 2: 1})}
+
+
 def axis_indices(axis, orient='clin'):
     """
     Returns a pair of indices corresponding to right/up for the given orientation.
@@ -21,25 +23,27 @@ def axis_indices(axis, orient='clin'):
     this_orient = Orient_map[orient]
     return (this_orient[0][axis], this_orient[1][axis])
 
+
 class Slicer:
     """
     The Slicer class.
-    
+
     When constructed, creates an array of world-space co-ordinates which represent the desired slice
-    
+
     Constructor Parameters:
-    
+
     - bbox -- :py:class:`~nanslice.box.Box` instance that defines the world-space bounding box that you want to slice
     - pos -- Position within the box to generate the slice through
     - axis -- Which axis you want to slice across. Either x/y/z or 0/1/2
     - samples -- Number of samples in the horizontal direction
     - orient --  'clin' or 'preclin'
     """
+
     def __init__(self, bbox, pos, axis, samples=64, orient='clin'):
         try:
-            ind_0 = Axis_map[axis] # If someone passed in x/y/z
+            ind_0 = Axis_map[axis]  # If someone passed in x/y/z
         except KeyError:
-            ind_0 = axis # Assume it was an integer
+            ind_0 = axis  # Assume it was an integer
 
         ind_1, ind_2 = axis_indices(ind_0, orient=orient)
         start = np.copy(bbox.start)
@@ -54,7 +58,8 @@ class Slicer:
                              dir_rt[:, None, None] * np.linspace(0, 1, samples)[None, :, None] +
                              dir_up[:, None, None] * np.linspace(0, 1, samples_up)[None, None, :])
         # This is the extent parameter for matplotlib
-        self.extent = (bbox.start[ind_1], bbox.end[ind_1], bbox.start[ind_2], bbox.end[ind_2])
+        self.extent = (bbox.start[ind_1], bbox.end[ind_1],
+                       bbox.start[ind_2], bbox.end[ind_2])
         self._tfm = None
         self._voxel_space = None
 
@@ -63,7 +68,7 @@ class Slicer:
         Returns an array of voxel space co-ordinates for this slice, which will be cached.
         If a subsequent call uses the same affine transform, the cached co-ordinates will be returned.
         If a new transform is passed in, then a fresh set of co-ordinates are calculated first.
-        
+
         Parameters:
 
         - tfm -- An affine transform that defines an images physical space (usually the .affine property of an nibabel image)
@@ -73,7 +78,8 @@ class Slicer:
             new_sz = np.prod(self._world_space.shape[1:])
             scale = np.mat(tfm[0:3, 0:3]).I
             offset = np.dot(-scale, tfm[0:3, 3]).T
-            isl = np.dot(scale, self._world_space.reshape([3, new_sz])) + offset[:]
+            isl = np.dot(scale, self._world_space.reshape(
+                [3, new_sz])) + offset[:]
             isl = np.array(isl).reshape(old_sz)
             self._voxel_space = isl
             self._tfm = tfm
@@ -89,7 +95,7 @@ class Slicer:
         - order -- Interpolation order. 1 is linear interpolation
         - scale -- Scale factor to multiply all voxel values by
         - volume -- If sampling 4D data, specify the desired volume
-        
+
         """
         physical = self.get_voxel_coords(img.affine)
         # Support timeseries by adding an extra co-ord specifying the volume
