f590932872fe87771b0c4e729cc1bcb0d50a6b4a
Tobias Wood <tobias@spinicist.org.uk>
2019-02-12 08:11:09 +0000

    ENH Adds transpose option

--- nanslice/nanslicer.py
+++ nanslice/nanslicer.py
@@ -49,6 +49,8 @@ from .box import Box
 from .slicer import Slicer, Axis_map
 from .slice_func import scale_clip
 from .layer import Layer, blend_layers
+
+
 def main(args=None):
     """
     The main function that is called from the command line.
@@ -57,35 +59,48 @@ def main(args=None):
 
     - args -- The command-line arguments. See module docstring or command-line help for a full list
     """
-    parser = argparse.ArgumentParser(description='Takes aesthetically pleasing slices through MR images')
+    parser = argparse.ArgumentParser(
+        description='Takes aesthetically pleasing slices through MR images')
     add_common_arguments(parser)
     parser.add_argument('output', help='Output image name', type=str)
-    parser.add_argument('--slice_rows', type=int, default=4, help='Number of rows of slices')
-    parser.add_argument('--slice_cols', type=int, default=5, help='Number of columns of slices')
-    parser.add_argument('--slice_axis', type=str, default='z', help='Axis to slice along (x/y/z)')
-    parser.add_argument('--three_axis', help='Make a 3 axis (x,y,z) plot', action='store_true')
-    parser.add_argument('--timeseries', action='store_true', help='Plot the same slice through each volume in a time-series')
-    parser.add_argument('--volume', type=int, default=0, help='Plot one volume from a timeseries')
+    parser.add_argument('--slice_rows', type=int, default=4,
+                        help='Number of rows of slices')
+    parser.add_argument('--slice_cols', type=int, default=5,
+                        help='Number of columns of slices')
+    parser.add_argument('--slice_axis', type=str, default='z',
+                        help='Axis to slice along (x/y/z)')
+    parser.add_argument(
+        '--three_axis', help='Make a 3 axis (x,y,z) plot', action='store_true')
+    parser.add_argument('--timeseries', action='store_true',
+                        help='Plot the same slice through each volume in a time-series')
+    parser.add_argument('--volume', type=int, default=0,
+                        help='Plot one volume from a timeseries')
     parser.add_argument('--slice_lims', type=float, nargs=2, default=(0.1, 0.9),
                         help='Slice between these limits along the axis, default=0.1 0.9')
-    parser.add_argument('--bar_pos', type=str, default='bottom', help='Position of color-bar (bottom / right)')
-    parser.add_argument('--figsize', type=float, nargs=2, default=None, help='Figure size (width, height) in inches')
-    parser.add_argument('--dpi', type=int, default=150, help='DPI for output figure')
+    parser.add_argument('--bar_pos', type=str, default='bottom',
+                        help='Position of color-bar (bottom / right)')
+    parser.add_argument('--figsize', type=float, nargs=2,
+                        default=None, help='Figure size (width, height) in inches')
+    parser.add_argument('--dpi', type=int, default=150,
+                        help='DPI for output figure')
+    parser.add_argument('--transpose', action='store_true',
+                        help='Swap rows and columns')
 
     args = parser.parse_args()
 
     print('*** Loading files')
     print('Loading base image: ', args.base_image)
-    layers = [Layer(args.base_image, cmap=args.base_map, clim=args.base_lims, mask=args.mask,
-                    interp_order=args.interp_order,volume=args.volume),]
+    layers = [Layer(args.base_image, mask=args.mask,
+                    cmap=args.base_map, clim=args.base_lims, scale=args.base_scale,
+                    interp_order=args.interp_order, volume=args.volume), ]
     if args.base_lims is None:
         print('Base limits:', layers[0].clim)
 
     if args.overlay:
-        layers.append(Layer(args.overlay, cmap=args.overlay_map, clim=args.overlay_lim,
+        layers.append(Layer(args.overlay, scale=args.overlay_scale,
+                            cmap=args.overlay_map, clim=args.overlay_lims,
                             mask=args.overlay_mask, mask_threshold=args.overlay_mask_thresh,
-                            alpha=args.overlay_alpha, alpha_lim=args.overlay_alpha_lim,
-                            alpha_scale=args.overlay_alpha_scale,
+                            alpha=args.alpha, alpha_scale=args.overlay_alpha_scale, alpha_lim=args.alpha_lims,
                             interp_order=args.interp_order))
 
     print('*** Setup')
@@ -103,9 +118,11 @@ def main(args=None):
         slice_total = layers[0].image.shape[3]
     else:
         slice_total = args.slice_rows*args.slice_cols
-        slice_pos = bbox.start[args.slice_axis] + bbox.diag[args.slice_axis] * np.linspace(args.slice_lims[0], args.slice_lims[1], slice_total)
+        slice_pos = bbox.start[args.slice_axis] + bbox.diag[args.slice_axis] * \
+            np.linspace(args.slice_lims[0], args.slice_lims[1], slice_total)
         args.slice_axis = [args.slice_axis] * slice_total
-    print(slice_total, ' slices in ', args.slice_rows, ' rows and ', args.slice_cols, ' columns')
+    print(slice_total, ' slices in ', args.slice_rows,
+          ' rows and ', args.slice_cols, ' columns')
 
     if args.orient == 'preclin':
         origin = 'upper'
@@ -116,11 +133,16 @@ def main(args=None):
     if args.figsize:
         f = plt.figure(facecolor='black', figsize=args.figsize)
     else:
-        f = plt.figure(facecolor='black', figsize=(3*args.slice_cols, 3*args.slice_rows))
+        f = plt.figure(facecolor='black', figsize=(
+            3*args.slice_cols, 3*args.slice_rows + 1))
 
     print('*** Slicing')
     for s in range(0, slice_total):
-        ax = plt.subplot(gs1[s], facecolor='black')
+        if args.transpose:
+            col, row = divmod(s, args.slice_rows)
+        else:
+            row, col = divmod(s, args.slice_cols)
+        ax = plt.subplot(gs1[row, col], facecolor='black')
         if args.timeseries:
             layers[0].volume = s
             sp = slice_pos
@@ -129,50 +151,61 @@ def main(args=None):
             sp = slice_pos[s]
             axis = args.slice_axis[s]
 
-        print('Slice pos ', sp)
         slcr = Slicer(bbox, sp, axis, args.samples, orient=args.orient)
         sl_final = blend_layers(layers, slcr)
-        ax.imshow(sl_final, origin=origin, extent=slcr.extent, interpolation=args.interp)
+        ax.imshow(sl_final, origin=origin, extent=slcr.extent,
+                  interpolation=args.interp)
         ax.axis('off')
         if args.contour:
             sl_contour = layers[1].get_alpha(slcr)
-            contour_levels = scale_clip(np.array(args.contour), args.overlay_alpha_lim)
+            contour_levels = scale_clip(
+                np.array(args.contour), args.overlay_alpha_lim)
 
             # Contour levels must be within the range of overlay alpha values.
             # Ignore contour levels that are not within this range to prevent
             # spurious contour lines from being drawn.
-            valid_levels = (np.min(sl_contour) < contour_levels) & (contour_levels < np.max(sl_contour))
+            valid_levels = (np.min(sl_contour) < contour_levels) & (
+                contour_levels < np.max(sl_contour))
             if any(valid_levels):
                 ax.contour(sl_contour, levels=contour_levels[valid_levels], origin=origin, extent=slcr.extent,
-                        colors=args.contour_color, linestyles=args.contour_style, linewidths=1)
+                           colors=args.contour_color, linestyles=args.contour_style, linewidths=1)
 
     if args.base_label or args.overlay_label:
         print('*** Adding colorbar')
         if args.bar_pos == 'bottom':
-            gs1.update(left=0.01, right=0.99, bottom=0.16, top=0.99, wspace=0.01, hspace=0.01)
+            gs1.update(left=0.01, right=0.99, bottom=(0.4 / (1 + args.slice_rows)),
+                       top=0.99, wspace=0.01, hspace=0.01)
             gs2 = gridspec.GridSpec(1, 1)
-            gs2.update(left=0.08, right=0.92, bottom=0.08, top=0.15, wspace=0.1, hspace=0.1)
-            orient='h'
+            gs2.update(left=0.08, right=0.92, bottom=(0.2 / (1 + args.slice_rows)),
+                       top=(0.38 / (1 + args.slice_rows)), wspace=0.1, hspace=0.1)
+            orient = 'h'
         else:
-            gs1.update(left=0.01, right=0.95, bottom=0.01, top=0.99, wspace=0.01, hspace=0.01)
+            gs1.update(left=0.01, right=0.95, bottom=0.01,
+                       top=0.99, wspace=0.01, hspace=0.01)
             gs2 = gridspec.GridSpec(1, 1)
-            gs2.update(left=0.97, right=0.99, bottom=0.05, top=0.95, wspace=0.01, hspace=0.01)
-            orient='v'
+            gs2.update(left=0.97, right=0.99, bottom=0.05,
+                       top=0.95, wspace=0.01, hspace=0.01)
+            orient = 'v'
         axes = plt.subplot(gs2[0], facecolor='black')
         if args.overlay_alpha:
-            alphabar(axes, args.overlay_map, args.overlay_lim, args.overlay_label,
-                        args.overlay_alpha_lim, args.overlay_alpha_label, orient=orient)
+            alphabar(axes, args.overlay_map, args.overlay_lims, args.overlay_label,
+                     args.overlay_alpha_lims, args.overlay_alpha_label, orient=orient)
         else:
             if args.base_map:
-                colorbar(axes, layers[0].cmap, layers[0].clim, args.base_label, orient=orient)
+                colorbar(axes, layers[0].cmap, layers[0].clim,
+                         args.base_label, orient=orient)
             else:
-                colorbar(axes, layers[1].cmap, layers[1].clim, args.overlay_label, orient=orient)
+                colorbar(axes, layers[1].cmap, layers[1].clim,
+                         args.overlay_label, orient=orient)
     else:
-        gs1.update(left=0.01, right=0.99, bottom=0.01, top=0.99, wspace=0.01, hspace=0.01)
+        gs1.update(left=0.01, right=0.99, bottom=0.01,
+                   top=0.99, wspace=0.01, hspace=0.01)
     print('*** Saving')
     print('Writing file: ', args.output, 'at', args.dpi, ' DPI')
-    f.savefig(args.output, facecolor=f.get_facecolor(), edgecolor='none', dpi=args.dpi)
+    f.savefig(args.output, facecolor=f.get_facecolor(),
+              edgecolor='none', dpi=args.dpi)
     plt.close(f)
+
 
 if __name__ == "__main__":
     main()
