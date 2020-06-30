94f6c77239014f4c855960420b43e052c336682e
Tobias Wood <tobias@mi3.com>
2020-06-30 11:30:48 +0100

    BLD Bump version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='nanslice',
-    version='1.0.0',
+    version='1.1',
     description='Scripts to slice and display neuroimages (probably stored in nifti format)',
     long_description=long_description,
     long_description_content_type="text/markdown",
